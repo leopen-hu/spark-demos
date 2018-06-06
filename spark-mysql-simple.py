f5ef35a78a4404ff7f88092104cc2b5b9e843d30
leopen <790480953@qq.com>
2018-06-06 14:52:43 +0800

    [leopen] use '()' to replace '\'

--- spark-mysql-simple.py
+++ spark-mysql-simple.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
       .config("spark.some.config.option", "some-value") \
       .getOrCreate()
 
-    df = spark.read \
-      .format("jdbc") \
-      .option("url", "jdbc:mysql://10.25.75.138/jigglypuff?charset=utf8") \
-      .option("dbtable", "wind_all_data") \
-      .option("user", "root") \
-      .option("password", "123123") \
-      .load()
+    df = (spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://10.25.75.138/jigglypuff?charset=utf8")
+      .option("dbtable", "wind_all_data")
+      .option("user", "root")
+      .option("password", "123123")
+      .load())
 
     # sql operate
     df.groupBy("metric").count().show()
